8f5cd7520647e44b9b8a3260f68f37b6cbcafa15
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2026-06-07 10:12:36 -0600

    barebones flask app

--- web/app.py
+++ web/app.py
@@ -1,1 +1,12 @@
 # Flask server: loads model on startup, serves API routes and Jinja2 frontend.
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return "hi"
+
+if __name__ == "__main__":
+	app.run()
